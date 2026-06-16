6b3926e4c98a461e65f66981a2bd322b051459b0
mukul patel <mukulpatel@mukuls-MacBook-Air.local>
2026-06-16 22:39:39 +0530

    day10

---
+++ day10/question37.java
@@ -0,0 +1,17 @@
+//write a program to print star pyramid pattern .
+package day10;
+
+public class question37 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
