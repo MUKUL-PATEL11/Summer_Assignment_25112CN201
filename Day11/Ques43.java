7ccc58073049ccd7fc0281c44755892bcc8116c4
mukul patel <mukulpatel@mukuls-MacBook-Air.local>
2026-06-25 21:30:55 +0530

    Day11

---
+++ Day11/Ques43.java
@@ -0,0 +1,30 @@
+package Day11;
+
+import java.util.Scanner;
+
+public class Ques43 {
+    public static boolean isPrime(int n){
+        if(n <= 1){
+            return false;
+        }
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int n = sc.nextInt();
+        if(isPrime(n)){
+            System.out.println(n + " is prime");
+        }else{
+            System.out.println(n + " is not prime");
+        }
+        sc.close();
+    }
+}
+
